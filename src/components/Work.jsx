d3040e25904c50492538ff8c1d2e84a7ac0b6654
Kiran Pokhrel <pokhrelkiran321@gmail.com>
2026-02-22 11:52:48 +0545

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -28,16 +28,16 @@ export default function Work() {
     return (
         <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
             <h4 className="text-center mb-2 text-lg font-Ovo">My portfolio</h4>
-            <h2 className="text-center text-5xl font-Ovo">My latest work</h2>
-            <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">Welcome to my web development portfolio! Explore a collection of projects showcasing my expertise in front-end development.</p>
+            <h2 className="text-center text-5xl font-Ovo">My Certificates</h2>
+            <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">Welcome to my personal portfolio.</p>
 
             <div className="grid grid-cols-auto my-10 gap-5 dark:text-black">
-                {work.map((work) => (
+                {Certificates.map((Certificates) => (
                     <div key={work.name} className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group" style={{ backgroundImage: `url(${work.icon})` }}>
                         <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
                             <div>
-                                <h2 className="font-semibold">{work.name}</h2>
-                                <p className="text-sm text-gray-700">{work.description}</p>
+                                <h2 className="font-semibold">{Certificates.name}</h2>
+                                <p className="text-sm text-gray-700">{Certificates.description}</p>
                             </div>
                             <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
                                 <img src="./assets/send-icon.png" alt="" className="w-5" />
