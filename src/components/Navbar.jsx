6375d23fc0d26f9f11d0cfbd7722a2d1c85bb86b
Kiran Pokhrel <pokhrelkiran321@gmail.com>
2026-02-22 10:40:27 +0545

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,7 +60,7 @@ export default function Navbar() {
                     <li><a className='hover:text-gray-500 dark:hover:text-gray-300 transition' href="#top">Home</a></li>
                     <li><a className='hover:text-gray-500 dark:hover:text-gray-300 transition' href="#about">About me</a></li>
                     <li><a className='hover:text-gray-500 dark:hover:text-gray-300 transition' href="#services">Services</a></li>
-                    <li><a className='hover:text-gray-500 dark:hover:text-gray-300 transition' href="#work">My Work</a></li>
+                    <li><a className='hover:text-gray-500 dark:hover:text-gray-300 transition' href="#work">Certificates</a></li>
                     <li><a className='hover:text-gray-500 dark:hover:text-gray-300 transition' href="#contact">Contact me</a></li>
                 </ul>
 
@@ -93,7 +93,7 @@ export default function Navbar() {
                     <li><a href="#top" onClick={closeMenu}>Home</a></li>
                     <li><a href="#about" onClick={closeMenu}>About me</a></li>
                     <li><a href="#services" onClick={closeMenu}>Services</a></li>
-                    <li><a href="#work" onClick={closeMenu}>My Work</a></li>
+                    <li><a href="#work" onClick={closeMenu}>Certificates</a></li>
                     <li><a href="#contact" onClick={closeMenu}>Contact me</a></li>
                 </ul>
             </nav>
